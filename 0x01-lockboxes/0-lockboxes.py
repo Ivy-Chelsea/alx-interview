75705e203e47163d979365cc115d5010f0a539cb
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2023-03-30 21:40:41 +0300

    add necessary whitespaces

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,18 +3,23 @@
 Defines a function that attemps to solve a lockbox challenge
 """
 
-def canUnlockAll(boxes):
-    """Determines if boxes can be unlocked"""
-    position = 0
-    unlocked = {}
 
-    for box in boxes:
-        if len(box) == 0 or position == 0:
-            unlocked[position] = "always_unlocked"
-        for key in box:
-            if key < len(boxes) and key != position:
-                unlocked[key] = key
-        if len(unlocked) == len(boxes):
-            return True
-        position += 1
-    return False
+def canUnlockAll(boxes):
+    """
+    Function that checks with boolean value if the list type and
+    length to invoke two for iterations. One to traverse the list
+    and the other to compare if key is idx or not in order to open
+    """
+    if type(boxes) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
