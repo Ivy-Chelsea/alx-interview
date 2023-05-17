2f2caa7100989f21530f0eabbc6264f50571654e
Ivy Chelsea <ivychelsea20@gmail.com>
2023-05-17 21:02:08 +0300

    fix mispelled variables

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -16,13 +16,13 @@ def makeChange(coins, total):
     coins_count = 0
     coin_idx = 0
     sorted_coins = sorted(coins, reverse=True)
-    n = len(coins)
-    while rem > 0
-        if coins_idx >= y:
+    y = len(coins)
+    while rem > 0:
+        if coin_idx >= y:
             return - 1
         if rem - sorted_coins[coin_idx] >= 0:
             rem -= sorted_coins[coin_idx]
             coins_count += 1
-        else
+        else:
             coin_idx += 1
     return coins_count
